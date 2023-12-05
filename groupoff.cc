05fa61c3229af1b63a8a3a228d0c3c3ebf49948f
Patrick He <patrick.zebing.he@gmail.com>
2023-12-05 16:04:44 -0500

    idk what im doing

--- groupoff.cc
+++ groupoff.cc
@@ -1,4 +1,5 @@
 #include "groupoff.h"
+
 #include "printer.h"
 
 Groupoff::Groupoff(Printer& prt, unsigned int numStudents,
@@ -6,17 +7,17 @@ Groupoff::Groupoff(Printer& prt, unsigned int numStudents,
     : printer(prt),
       numStudents(numStudents),
       sodaCost(sodaCost),
-      groupoffDelay(groupoffDelay) {};
+      groupoffDelay(groupoffDelay){};
 
 WATCard::FWATCard Groupoff::giftCard() {
-    WATCard::FWATCard futureGiftCard;
-    futGiftCards.push_back(futureGiftCard);
-    return futureGiftCard;
+    bench.wait();
+
+    return futGiftCards.back();
 }
 
 Groupoff::~Groupoff() {
     for (int i = 0; i < legitWATCards.size(); i++) {
-        delete legitWATCards.at(i); // dealloc
+        delete legitWATCards.at(i);  // dealloc
     }
 }
 
@@ -24,25 +25,30 @@ void Groupoff::main() {
     printer.print(Printer::Groupoff, Groupoff::States::Start);
 
     for (unsigned int i = 0; i < numStudents; i++) {
-        _Accept(giftCard){};
+        _Accept(giftCard) {
+            WATCard::FWATCard futureGiftCard;
+            futGiftCards.push_back(futureGiftCard);
+            bench.signal();
+        };
     }
 
     for (unsigned int i = 0; i < numStudents; i++) {
-        _Accept(~Groupoff) { break; } 
+        _Accept(~Groupoff) { break; }
         _Else {
             // give a rando a giftcard
             yield(groupoffDelay);
 
-            WATCard * w = new WATCard(); 
+            WATCard* w = new WATCard();
             w->deposit(sodaCost);
             legitWATCards.push_back(w);
             unsigned int student_idx = my_prng(futGiftCards.size());
-            
+
             futGiftCards[student_idx].delivery(w);
 
             futGiftCards.erase(futGiftCards.begin() + student_idx);
             // print after delivery
-            printer.print(Printer::Kind::Groupoff, Groupoff::States::DepositGift, sodaCost);
+            printer.print(Printer::Kind::Groupoff,
+                          Groupoff::States::DepositGift, sodaCost);
         }
     }
     printer.print(Printer::Groupoff, Groupoff::States::Finished);
